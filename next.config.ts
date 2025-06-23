b9f9c23eb39fc1c98473ec800b36d7c428110fe1
Abdun Noor Faruki Biswas <dev.abdunnoor@gmail.com>
2025-06-23 15:27:05 +0530

    logout issue fixed.

--- next.config.ts
+++ next.config.ts
@@ -13,30 +13,30 @@ export default () => {
       NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
       NEXT_PUBLIC_GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     },
-    async headers() {
-      return [
-        {
-          // Apply these headers to all routes
-          source: "/api/:path*",
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            {
-              key: "Access-Control-Allow-Origin",
-              value: process.env.NEXT_PUBLIC_AUTH_BASE_URL!,
-            },
-            {
-              key: "Access-Control-Allow-Methods",
-              value: "GET,DELETE,PATCH,POST,PUT,OPTIONS",
-            },
-            {
-              key: "Access-Control-Allow-Headers",
-              value:
-                "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
-            },
-          ],
-        },
-      ];
-    },
+    // async headers() {
+    //   return [
+    //     {
+    //       // Apply these headers to all routes
+    //       source: "/api/:path*",
+    //       headers: [
+    //         { key: "Access-Control-Allow-Credentials", value: "true" },
+    //         {
+    //           key: "Access-Control-Allow-Origin",
+    //           value: process.env.NEXT_PUBLIC_AUTH_BASE_URL!,
+    //         },
+    //         {
+    //           key: "Access-Control-Allow-Methods",
+    //           value: "GET,DELETE,PATCH,POST,PUT,OPTIONS",
+    //         },
+    //         {
+    //           key: "Access-Control-Allow-Headers",
+    //           value:
+    //             "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
+    //         },
+    //       ],
+    //     },
+    //   ];
+    // },
   };
   return nextConfig;
 };
